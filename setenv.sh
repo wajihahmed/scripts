0215c19572801a0bda075755d90bee707475c108
Wajih Ahmed <wajih_ahmed@yahoo.com>
2015-04-14 12:51:51 -0400

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,2 @@
 JAVA_OPTS="-server -d64 -Xms1024m -Xmx1024m -XX:MaxPermSize=128m -XX:+UseParNewGC -XX:MaxNewSize=256m -XX:NewSize=256m -XX:MaxTenuringThreshold=1 -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:/tmp/amgc.log -XX:+PrintTenuringDistribution"
-
 #JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n"
