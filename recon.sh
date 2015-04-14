9c8bcee61fdb9f480f613f417470c670f78acd02
Wajih Ahmed <wajih_ahmed@yahoo.com>
2015-04-14 12:51:30 -0400

    Delete recon.sh

--- recon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -k -H "Content-type: application/json" -u "openidm-admin:openidm-admin" -X POST "http://localhost:9080/openidm/recon?_action=recon&mapping=systemLdapAccounts_managedUser"
