0486d7ca2ddcfda001b0393b9194953fb12f5d13
Wajih Ahmed <wajih_ahmed@yahoo.com>
2015-04-14 12:48:03 -0400

    Delete idm_chg_port.sh

--- idm_chg_port.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-find . -name 'boot.properties' -exec perl -pi.orig -e 's/8080/9080/g ; s/8443/9443/g ; s/8444/9444/g' {} \;
-find . -name 'ui-configuration.json' -exec perl -pi.orig -e 's/\"selfRegistration\"\s:\sfalse,/\"selfRegistration\"\s:\strue,/g ; 's/\"securityQuestions\"\s:\strue,/g'
-
-
