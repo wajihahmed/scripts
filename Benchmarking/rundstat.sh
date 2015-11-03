70e8218605fc4a18b835a5090088e12fd71389d8
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2015-11-03 08:56:49 -0500

    Added more details

--- Benchmarking/rundstat.sh
+++ Benchmarking/rundstat.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-# need to run as sudo because of bug in netstat
-
-sudo dstat -tcmndr --integer --output /home/fr/mon/dstat.m1-$1 5
+dstat -tcmndr --integer --output /home/fr/mon/dstat.m1-$1 5
